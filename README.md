86110445b9e66d1fff05eb97d69a63a407e40807
Nicolas Schoofs <s130014@ap.be>
2025-01-31 14:05:21 +0100

    temp

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 dees is een fucking hype project
+
+
+home
+
+
+planning pagina
+ -> kalender 7 dagen
+
+klanten/orders opzoeken
+ -> klanten lijst
+ -> add klanten
+ -> remove klanten
+
+klanten info pagina
+ -> orders
+ -> geplande meetings
---
+++ frontend/src/Users.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { Table } from 'react-bootstrap'
+
+//import './Users.css'
+
+function Users() {
+
+    return (
+        <>
+            <Table className="table table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
+    )
+}
+
+export default Users
